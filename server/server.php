3faefc5728ca0315a764dfa88e46f0692596e38d
扬 宋 <madcloud@yangs-iMac.local>
2015-06-08 13:27:29 +0800

    debug websocket

--- server/server.php
+++ server/server.php
@@ -23,7 +23,7 @@ $ws->on('message', function ($ws, $frame) {
     $ws->push($frame->fd, "server: {$frame->data}");
 });
 
-$server->on('close', function ($ws, $fd) {
+$ws->on('close', function ($ws, $fd) {
     echo "client {$fd} closed\n";
 });
 
